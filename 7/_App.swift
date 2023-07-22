bc2a8a1eacb08daa24e07238b4d85aaab5187c8e
Nicole Tan <geniusnic009@gmail.com>
2023-07-22 10:12:04 +0800

    Initial Commit

---
+++ 7/_App.swift
@@ -0,0 +1,17 @@
+//
+//  _App.swift
+//  7
+//
+//  Created by T Krobot on 22/7/23.
+//
+
+import SwiftUI
+
+@main
+struct _App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
